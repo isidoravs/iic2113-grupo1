7eab47497fcb7535f332dd3632cb9b57b69b3579
ftvannievelt <ftvannievelt@uc.cl>
2019-09-29 22:54:03 -0300

    create room model

---
+++ ConferenceApp/Models/Room.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConferenceApp.Models
+{
+  public class Room
+  {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int MaxCapacity { get; set; }
+        public string[] Equipment { get; set; }
+    }
+}
