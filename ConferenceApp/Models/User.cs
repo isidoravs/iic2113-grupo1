feb52efe1f3c9aab0dee77567fc2ce2e329c5eca
FranciscoLarach <fjlarach@uc.cl>
2019-10-03 22:29:17 -0300

    User Inheritance from IdentityUser

--- ConferenceApp/Models/User.cs
+++ ConferenceApp/Models/User.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace ConferenceApp.Models
 {
-    public class User
+    public class User : IdentityUser
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
