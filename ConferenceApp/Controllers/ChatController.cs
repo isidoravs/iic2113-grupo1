78fbffce4a7b38cda17ddf4bd61373670769d9c2
Isidora Vizcaya <isvizcaya@uc.cl>
2019-11-10 19:22:47 -0300

    add chat controller scaffold

---
+++ ConferenceApp/Controllers/ChatController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ConferenceApp.Data;
+using ConferenceApp.Models;
+
+namespace ConferenceApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ChatController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Chat
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Chats.ToListAsync());
+        }
+
+        // GET: Chat/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chat = await _context.Chats
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (chat == null)
+            {
+                return NotFound();
+            }
+
+            return View(chat);
+        }
+
+        // GET: Chat/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Chat/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Topic,Id,Name,StartDate,EndDate,ConferenceVersionId")] Chat chat)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(chat);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(chat);
+        }
+
+        // GET: Chat/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chat = await _context.Chats.FindAsync(id);
+            if (chat == null)
+            {
+                return NotFound();
+            }
+            return View(chat);
+        }
+
+        // POST: Chat/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Topic,Id,Name,StartDate,EndDate,ConferenceVersionId")] Chat chat)
+        {
+            if (id != chat.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(chat);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ChatExists(chat.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(chat);
+        }
+
+        // GET: Chat/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chat = await _context.Chats
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (chat == null)
+            {
+                return NotFound();
+            }
+
+            return View(chat);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var chat = await _context.Chats.FindAsync(id);
+            _context.Chats.Remove(chat);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ChatExists(int id)
+        {
+            return _context.Chats.Any(e => e.Id == id);
+        }
+    }
+}
