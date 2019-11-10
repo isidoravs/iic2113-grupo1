784652343adb73a696ed2eca150195cda813cb91
Isidora Vizcaya <isvizcaya@uc.cl>
2019-11-10 17:49:28 -0300

    add practical session controller scaffold

---
+++ ConferenceApp/Controllers/PracticalSessionController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ConferenceApp.Data;
+using ConferenceApp.Models;
+
+namespace ConferenceApp.Controllers
+{
+    public class PracticalSessionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PracticalSessionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PracticalSession
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.PracticalSessions.ToListAsync());
+        }
+
+        // GET: PracticalSession/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var practicalSession = await _context.PracticalSessions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (practicalSession == null)
+            {
+                return NotFound();
+            }
+
+            return View(practicalSession);
+        }
+
+        // GET: PracticalSession/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PracticalSession/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Topic,ComplementaryMaterial,Id,Name,StartDate,EndDate,ConferenceVersionId")] PracticalSession practicalSession)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(practicalSession);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(practicalSession);
+        }
+
+        // GET: PracticalSession/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var practicalSession = await _context.PracticalSessions.FindAsync(id);
+            if (practicalSession == null)
+            {
+                return NotFound();
+            }
+            return View(practicalSession);
+        }
+
+        // POST: PracticalSession/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Topic,ComplementaryMaterial,Id,Name,StartDate,EndDate,ConferenceVersionId")] PracticalSession practicalSession)
+        {
+            if (id != practicalSession.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(practicalSession);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PracticalSessionExists(practicalSession.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(practicalSession);
+        }
+
+        // GET: PracticalSession/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var practicalSession = await _context.PracticalSessions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (practicalSession == null)
+            {
+                return NotFound();
+            }
+
+            return View(practicalSession);
+        }
+
+        // POST: PracticalSession/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var practicalSession = await _context.PracticalSessions.FindAsync(id);
+            _context.PracticalSessions.Remove(practicalSession);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PracticalSessionExists(int id)
+        {
+            return _context.PracticalSessions.Any(e => e.Id == id);
+        }
+    }
+}
