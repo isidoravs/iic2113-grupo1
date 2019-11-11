7b38194d8b045a1f9cbf6ead684383c8285d88bd
Isidora Vizcaya <isvizcaya@uc.cl>
2019-11-10 21:28:43 -0300

    add party controller scaffold

---
+++ ConferenceApp/Controllers/PartyController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ConferenceApp.Data;
+using ConferenceApp.Models;
+
+namespace ConferenceApp.Controllers
+{
+    public class PartyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PartyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Party
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Parties.ToListAsync());
+        }
+
+        // GET: Party/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var party = await _context.Parties
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (party == null)
+            {
+                return NotFound();
+            }
+
+            return View(party);
+        }
+
+        // GET: Party/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Party/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MusicStyle,Id,Name,StartDate,EndDate,ConferenceVersionId")] Party party)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(party);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(party);
+        }
+
+        // GET: Party/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var party = await _context.Parties.FindAsync(id);
+            if (party == null)
+            {
+                return NotFound();
+            }
+            return View(party);
+        }
+
+        // POST: Party/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MusicStyle,Id,Name,StartDate,EndDate,ConferenceVersionId")] Party party)
+        {
+            if (id != party.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(party);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PartyExists(party.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(party);
+        }
+
+        // GET: Party/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var party = await _context.Parties
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (party == null)
+            {
+                return NotFound();
+            }
+
+            return View(party);
+        }
+
+        // POST: Party/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var party = await _context.Parties.FindAsync(id);
+            _context.Parties.Remove(party);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PartyExists(int id)
+        {
+            return _context.Parties.Any(e => e.Id == id);
+        }
+    }
+}
--- ConferenceApp/Data/ApplicationDbContext.cs
+++ ConferenceApp/Data/ApplicationDbContext.cs
@@ -19,6 +19,7 @@ namespace ConferenceApp.Data
         public DbSet<FeedbackScope> FeedbackScopes { get; set; }
         public DbSet<Feedback> Feedbacks { get; set; }
         public DbSet<FoodService> FoodServices { get; set; }
+        public DbSet<Party> Parties { get; set; }
         public DbSet<PracticalSession> PracticalSessions { get; set; }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Sponsor> Sponsors { get; set; }
@@ -29,6 +30,5 @@ namespace ConferenceApp.Data
             : base(options)
         {
         }
-        public DbSet<ConferenceApp.Models.EventCentre> EventCentre { get; set; }
     }
 }
