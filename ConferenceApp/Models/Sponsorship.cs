eab2521a268cf2d8db5cdfd7ea5fb69a77053cc9
Isidora Vizcaya <isvizcaya@uc.cl>
2019-11-19 13:23:11 -0300

    add sponsorship model

--- ConferenceApp/Models/Sponsorship.cs
+++ ConferenceApp/Models/Sponsorship.cs
@@ -1,16 +1,16 @@
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ConferenceApp.Models
 {
-    public class Sponsor
+    public class Sponsorship
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public string Name { get; set; }
-        
-        public ICollection<Sponsorship> Sponsorships { get; set; }
+        public int ConferenceVersionId { get; set; }
+        public int SponsorId { get; set; }
     }
 }
