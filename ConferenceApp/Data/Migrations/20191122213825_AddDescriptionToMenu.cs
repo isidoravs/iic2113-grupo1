5ed62412bfd488205d2060ae3681ca0a5bb97e20
Isidora Vizcaya <isvizcaya@uc.cl>
2019-11-22 18:39:02 -0300

    add name to menu option

---
+++ ConferenceApp/Data/Migrations/20191122213825_AddDescriptionToMenu.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConferenceApp.Data.Migrations
+{
+    public partial class AddDescriptionToMenu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MenuOptions",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MenuOptions");
+        }
+    }
+}
--- ConferenceApp/Models/MenuOption.cs
+++ ConferenceApp/Models/MenuOption.cs
@@ -11,6 +11,8 @@ namespace ConferenceApp.Models
         public int Id { get; set; }
         [Required]
         
+        public string Name { get; set; }
+        
         public int FoodServiceId { get; set; }
     }
 }
