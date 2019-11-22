f6d2d5d4767dbea2250f9f58ea70debd504abdeb
Isidora Vizcaya <isvizcaya@uc.cl>
2019-11-22 18:33:32 -0300

    add menu options

---
+++ ConferenceApp/Controllers/MenuOptionController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ConferenceApp.Data;
+using ConferenceApp.Models;
+
+namespace ConferenceApp.Controllers
+{
+    public class MenuOptionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MenuOptionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MenuOption
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.MenuOptions.ToListAsync());
+        }
+
+        // GET: MenuOption/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menuOption = await _context.MenuOptions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menuOption == null)
+            {
+                return NotFound();
+            }
+
+            return View(menuOption);
+        }
+
+        // GET: MenuOption/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MenuOption/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FoodServiceId")] MenuOption menuOption)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(menuOption);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menuOption);
+        }
+
+        // GET: MenuOption/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menuOption = await _context.MenuOptions.FindAsync(id);
+            if (menuOption == null)
+            {
+                return NotFound();
+            }
+            return View(menuOption);
+        }
+
+        // POST: MenuOption/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FoodServiceId")] MenuOption menuOption)
+        {
+            if (id != menuOption.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(menuOption);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MenuOptionExists(menuOption.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menuOption);
+        }
+
+        // GET: MenuOption/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menuOption = await _context.MenuOptions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menuOption == null)
+            {
+                return NotFound();
+            }
+
+            return View(menuOption);
+        }
+
+        // POST: MenuOption/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var menuOption = await _context.MenuOptions.FindAsync(id);
+            _context.MenuOptions.Remove(menuOption);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MenuOptionExists(int id)
+        {
+            return _context.MenuOptions.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ConferenceApp/Models/MenuOption.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConferenceApp.Models
+{
+    public class MenuOption
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        
+        public int FoodServiceId { get; set; }
+    }
+}
