3cd743660e034e124adf55bec5bb94f5fcecfed4
Isidora Vizcaya <isvizcaya@uc.cl>
2019-12-05 18:12:07 -0300

    reset feedback controller

--- ConferenceApp/Controllers/FeedbackController.cs
+++ ConferenceApp/Controllers/FeedbackController.cs
@@ -54,7 +54,7 @@ namespace ConferenceApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Message")] Feedback feedback)
+        public async Task<IActionResult> Create([Bind("Id,Message,EventId")] Feedback feedback)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace ConferenceApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Message")] Feedback feedback)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Message,EventId")] Feedback feedback)
         {
             if (id != feedback.Id)
             {
